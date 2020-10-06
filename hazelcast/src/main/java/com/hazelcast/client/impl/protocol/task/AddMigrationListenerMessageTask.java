3a5b61ee54d8c6b5eed0450070bac9997f320e8f
petjot <piotrekjasina@gmail.com>
2020-10-07 00:40:58 +0200

    implement add migration listener task

--- hazelcast/src/main/java/com/hazelcast/client/impl/protocol/task/AddMigrationListenerMessageTask.java
+++ hazelcast/src/main/java/com/hazelcast/client/impl/protocol/task/AddMigrationListenerMessageTask.java
@@ -18,27 +18,19 @@ package com.hazelcast.client.impl.protocol.task;
 
 import com.hazelcast.client.impl.protocol.ClientMessage;
 import com.hazelcast.client.impl.protocol.codec.ClientAddMigrationListenerCodec;
-import com.hazelcast.client.impl.protocol.codec.ClientAddPartitionLostListenerCodec;
-import com.hazelcast.cluster.Address;
-import com.hazelcast.cluster.Member;
-import com.hazelcast.cluster.impl.MemberImpl;
 import com.hazelcast.instance.impl.Node;
-import com.hazelcast.internal.cluster.ClusterService;
-import com.hazelcast.internal.cluster.impl.ClusterServiceImpl;
 import com.hazelcast.internal.nio.Connection;
 import com.hazelcast.internal.partition.IPartitionService;
 import com.hazelcast.partition.MigrationListener;
 import com.hazelcast.partition.MigrationState;
-import com.hazelcast.partition.PartitionLostListener;
 import com.hazelcast.partition.ReplicaMigrationEvent;
+import org.jetbrains.annotations.NotNull;
 
 import java.security.Permission;
 import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
 
 import static com.hazelcast.internal.partition.InternalPartitionService.MIGRATION_EVENT_TOPIC;
-import static com.hazelcast.internal.partition.InternalPartitionService.PARTITION_LOST_EVENT_TOPIC;
-import static com.hazelcast.spi.impl.InternalCompletableFuture.newCompletedFuture;
 
 public class AddMigrationListenerMessageTask
         extends AbstractAddListenerMessageTask<Boolean> {
@@ -53,34 +45,51 @@ public class AddMigrationListenerMessageTask
 
 
         final MigrationListener listener = new MigrationListener() {
+
             @Override
             public void migrationStarted(MigrationState state) {
+                // ignored
             }
 
             @Override
             public void migrationFinished(MigrationState state) {
+                // ignored
             }
 
             @Override
             public void replicaMigrationCompleted(ReplicaMigrationEvent event) {
-                final MigrationState migrationState = event.getMigrationState();
-                final ClientMessage clientMessage = ClientAddMigrationListenerCodec.encodeMigrationEvent(migrationState.getStartTime(), migrationState.getPlannedMigrations(),
-                        migrationState.getCompletedMigrations(), migrationState.getRemainingMigrations(), migrationState.getTotalElapsedTime(),
-                        true);
-                sendClientMessage(null, clientMessage);
+                if (endpoint.isAlive()) {
+                    ClientMessage eventMessage = encodeMigrationEvent(event, true);
+                    sendClientMessage(null, eventMessage);
+                }
             }
 
             @Override
             public void replicaMigrationFailed(ReplicaMigrationEvent event) {
-                final MigrationState migrationState = event.getMigrationState();
-                final ClientMessage clientMessage = ClientAddMigrationListenerCodec.encodeMigrationEvent(migrationState.getStartTime(), migrationState.getPlannedMigrations(),
-                        migrationState.getCompletedMigrations(), migrationState.getRemainingMigrations(), migrationState.getTotalElapsedTime(),
-                        false);
-                sendClientMessage(null, clientMessage);
+                if (endpoint.isAlive()) {
+                    ClientMessage eventMessage = encodeMigrationEvent(event, false);
+                    sendClientMessage(null, eventMessage);
+                }
             }
         };
 
         return partitionService.addMigrationListenerAsync(listener);
+    }
+
+    @NotNull
+    private ClientMessage encodeMigrationEvent(ReplicaMigrationEvent event, boolean success) {
+        return ClientAddMigrationListenerCodec.encodeMigrationEvent(
+                event.getMigrationState().getStartTime(),
+                event.getMigrationState().getPlannedMigrations(),
+                event.getMigrationState().getCompletedMigrations(),
+                event.getMigrationState().getTotalElapsedTime(),
+                event.getPartitionId(),
+                event.getReplicaIndex(),
+                event.getSource().getUuid(),
+                event.getDestination().getUuid(),
+                success,
+                event.getElapsedTime()
+        );
     }
 
     @Override
